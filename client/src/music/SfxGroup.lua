4f53d7e3a75ec0bf340f55dfe9b78962506b905a
Endaris <Endaris@users.noreply.github.com>
2024-12-28 17:18:20 +0100

    finish FileGroup and SfxGroup more or less?

--- client/src/music/SfxGroup.lua
+++ client/src/music/SfxGroup.lua
@@ -2,21 +2,70 @@ local class = require("common.lib.class")
 local tableUtils = require("common.lib.tableUtils")
 local FileUtils = require("client.src.FileUtils")
 
+-- A group of SFX that belong together
+-- Only 1 SFX in the group may play at the same time
+-- playing another SFX from the group will interrupt other ongoing SFX
 ---@class SfxGroup
----@field path string
----@field pattern string The pattern to search for as plain text; luaregex disabled
----@field separator string the separator by which alternative copies separate their index, default ""
----@field matchingFiles string[] the files that got matched during the load process of the SfxGroup
+---@field fileGroup FileGroup
+---@field sources love.Source[]
+---@field volumeMultiplier number
+---@field lastPlaying love.Source
+---@overload fun(fileGroup: FileGroup, volumeMultiplier: number?): SfxGroup
 local SfxGroup = class(
-function(self, path, pattern, separator)
-  self.path = path
-  self.pattern = pattern
-  self.separator = separator or ""
+---@param fileGroup FileGroup
+function(self, fileGroup, volumeMultiplier)
+  self.fileGroup = fileGroup
+  self.volumeMultiplier = volumeMultiplier or 1
+
+  -- fileGroup.indexedFiles may have gaps; this may be relevant to a SfxGroupGroup but not for a SfxGroup, just use all files
+  local continuouslyIndexedFiles = tableUtils.toContinuouslyIndexedTable(fileGroup.indexedFiles)
+  self.sources = {}
+  -- if there are gaps in indexedFiles, tough luck, they'll get ignored
+  for i, filename in ipairs(continuouslyIndexedFiles) do
+    self.sources[i] = love.audio.newSource(fileGroup.path .. "/" .. filename, "static")
+  end
 end)
 
-function SfxGroup:load(yields)
-  local files = FileUtils.getFilteredDirectoryItems(self.path, "file")
-  self.matchingFiles = FileUtils.getMatchingFiles(files, self.pattern, FileUtils.SUPPORTED_SOUND_FORMATS, self.separator)
+function SfxGroup:setVolume(volume)
+  for _, source in ipairs(self.sources) do
+    source:setVolume(volume * self.volumeMultiplier)
+  end
+end
+
+function SfxGroup:play()
+  if self.lastPlaying then
+    self.lastPlaying:stop()
+  end
+
+  self.lastPlaying = self.sources[math.random(#self.sources)]
+  self.lastPlaying:play()
+end
+
+function SfxGroup:isPlaying()
+  if not self.lastPlaying then
+    return false
+  else
+    return self.lastPlaying:isPlaying()
+  end
+end
+
+function SfxGroup:stop()
+  if self.lastPlaying then
+    self.lastPlaying:stop()
+  end
+end
+
+function SfxGroup:clone()
+---@diagnostic disable-next-line: param-type-mismatch
+  local clone = setmetatable({}, SfxGroup)
+  clone.fileGroup = self.fileGroup
+  clone.volumeMultiplier = self.volumeMultiplier
+  clone.sources = {}
+  for i, source in ipairs(self.sources) do
+    clone.sources[i] = source:clone()
+  end
+
+  return clone
 end
 
 return SfxGroup
