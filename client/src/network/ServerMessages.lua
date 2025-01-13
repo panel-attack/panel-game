55a81950d98a87b7cea795720271fc531268d126
Endaris <Endaris@users.noreply.github.com>
2025-01-13 20:27:58 +0100

    fix gameResult not being parsed into the expected format

--- client/src/network/ServerMessages.lua
+++ client/src/network/ServerMessages.lua
@@ -84,6 +84,7 @@ function ServerMessages.sanitizeRoomMessage(message)
   if message.type == "leaveRoom" then
     return {leave_room = true, reason = message.content.reason}
   elseif message.type == "gameResult" then
+    message.content.ratingInfo = message.content.rating
     return { gameResult = message.content }
   elseif message.type == "matchStart" then
     ---@type Replay
