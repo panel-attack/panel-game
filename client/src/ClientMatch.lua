ce2a8ae64749b0272dfc1fd17e225b87eb8f2a33
Endaris <Endaris@users.noreply.github.com>
2025-05-07 20:15:13 +0200

    fix ranked display not showing for spectators

--- client/src/ClientMatch.lua
+++ client/src/ClientMatch.lua
@@ -640,7 +640,7 @@ function ClientMatch:render()
     end
 
     -- Draw VS HUD
-    if self.stackInteraction == GameModes.StackInteractions.VERSUS then
+    if self.stackInteraction == GameModes.StackInteractions.VERSUS or self.replay.metadata.gameModeName == "VS" then
       if tableUtils.trueForAll(self.players, MatchParticipant.isHuman) or self.ranked then
         self:drawMatchType()
       end
