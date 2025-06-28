b17c8e2f13dc77144666a43190449623703a5781
Endaris <Endaris@users.noreply.github.com>
2025-06-29 00:38:37 +0200

    disable WigglePay for move puzzles

--- common/engine/Puzzle.lua
+++ common/engine/Puzzle.lua
@@ -246,6 +246,8 @@ function Puzzle:toGameMode()
     mode.matchRules.stackSetupModifications.behaviours = {
       allowManualRaise = false,
       passiveRaise = false,
+      swapStallingMode = 0,
+      swapStallingPunish = 0,
     }
     if self.puzzleType == "chain" then
       mode.matchRules.stackOverConditions[MatchRules.StackOverConditions.CHAIN] = false
