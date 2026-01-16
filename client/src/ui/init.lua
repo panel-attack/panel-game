45e50018b7805a7259180e4a711b78c9459c3541
Endaris <Endaris@users.noreply.github.com>
2026-01-16 22:15:46 +0100

    add an import function and a supporting plugin for the language server to facilitate easier navigation in the UI "namespace"

--- client/src/ui/init.lua
+++ client/src/ui/init.lua
@@ -1,57 +1,70 @@
-local PATH = (...):gsub('%.init$', '')
+local import = require("common.lib.import")
+
+--[[
+tag each with
+---@source relative path
+that way "Go to source" on an import of ui elsewhere will lead to the respective source instead of this file
+the "./" is assumed given for relative paths but it's still a path so adding the file extension is necessary
+when addressing files in subdirectories of ui use forward slashes as the path separator
+https://luals.github.io/wiki/annotations/#source
+]]
+
 
 local ui = {
-  ---@see BoolSelector
-  ---@type fun(options: BoolSelectorOptions): BoolSelector
-  BoolSelector = require(PATH .. ".BoolSelector"),
-  ---@see Button
-  ---@type fun(options: ButtonOptions): Button
-  Button = require(PATH .. ".Button"),
-  ButtonGroup = require(PATH .. ".ButtonGroup"),
-  Carousel = require(PATH .. ".Carousel"),
-  Focusable = require(PATH .. ".Focusable"),
-  FocusDirector = require(PATH .. ".FocusDirector"),
-  Grid = require(PATH .. ".Grid"),
-  GridCursor = require(PATH .. ".GridCursor"),
-  ---@see ImageButton
-  ---@type fun(options: ImageButtonOptions): ImageButton
-  ImageButton = require(PATH .. ".ImageButton"),
-  ImageContainer = require(PATH .. ".ImageContainer"),
-  InputField = require(PATH .. ".InputField"),
-  ---@see Label
-  ---@type fun(options: LabelOptions): Label
-  Label = require(PATH .. ".Label"),
-  Leaderboard = require(PATH .. ".Leaderboard"),
-  ---@see LevelSlider
-  ---@type fun(options: SliderOptions): LevelSlider
-  LevelSlider = require(PATH .. ".LevelSlider"),
-  Menu = require(PATH .. ".Menu"),
-  MenuItem = require(PATH .. ".MenuItem"),
-  MultiPlayerSelectionWrapper = require(PATH .. ".MultiPlayerSelectionWrapper"),
-  PagedUniGrid = require(PATH .. ".PagedUniGrid"),
-  PanelCarousel = require(PATH .. ".PanelCarousel"),
-  ---@see PixelFontLabel
-  ---@type fun(options: PixelFontLabelOptions): PixelFontLabel
-  PixelFontLabel = require(PATH .. ".PixelFontLabel"),
-  ---@see ScrollContainer
-  ---@type fun(options: ScrollContainerOptions): ScrollContainer
-  ScrollContainer = require(PATH .. ".ScrollContainer"),
-  ScrollText = require(PATH .. ".ScrollText"),
-  ---@see Slider
-  ---@type fun(options: SliderOptions): Slider
-  Slider = require(PATH .. ".Slider"),
+  ---@source BoolSelector.lua
+  BoolSelector = import("./BoolSelector"),
+  ---@source Button.lua
+  Button = import("./Button"),
+  ---@source ButtonGroup.lua
+  ButtonGroup = import("./ButtonGroup"),
+  Carousel = import("./Carousel"),
+  Focusable = import("./Focusable"),
+  FocusDirector = import("./FocusDirector"),
+  Grid = import("./Grid"),
+  GridCursor = import("./GridCursor"),
+  ---@source ImageButton.lua
+  ImageButton = import("./ImageButton"),
+  ---@source ImageContainer.lua
+  ImageContainer = import("./ImageContainer"),
+  ---@source InputField.lua
+  InputField = import("./InputField"),
+  ---@source Label.lua
+  ---@type Label
+  Label = import("./Label"),
+  Leaderboard = import("./Leaderboard"),
+  ---@source LevelSlider.lua
+  ---@type LevelSlider
+  LevelSlider = import("./LevelSlider"),
+  ---@source Menu.lua
+  Menu = import("./Menu"),
+  ---@source MenuItem.lua
+  MenuItem = import("./MenuItem"),
+  MultiPlayerSelectionWrapper = import("./MultiPlayerSelectionWrapper"),
+  PagedUniGrid = import("./PagedUniGrid"),
+  ---@source PanelCarousel.lua
+  PanelCarousel = import("./PanelCarousel"),
+  ---@source PixelFontLabel.lua
+  ---@type PixelFontLabel
+  PixelFontLabel = import("./PixelFontLabel"),
+  ---@source ScrollContainer.lua
+  ---@type ScrollContainer
+  ScrollContainer = import("./ScrollContainer"),
+  ScrollText = import("./ScrollText"),
+  ---@source Slider.lua
+  ---@type Slider
+  Slider = import("./Slider"),
   ---@source StackElement.lua
-  StackElement = require(PATH .. ".StackElement"),
-  StackPanel = require(PATH .. ".StackPanel"),
-  StageCarousel = require(PATH .. ".StageCarousel"),
-  Stepper = require(PATH .. ".Stepper"),
-  ---@see TextButton
-  ---@type fun(options: TextButtonOptions): TextButton
-  TextButton = require(PATH .. ".TextButton"),
-  ---@see UiElement
-  ---@type fun(options:UiElementOptions): UiElement
-  UiElement = require(PATH .. ".UIElement"),
-  ValueLabel = require(PATH .. ".ValueLabel"),
+  StackElement = import("./StackElement"),
+  StackPanel = import("./StackPanel"),
+  StageCarousel = import("./StageCarousel"),
+  Stepper = import("./Stepper"),
+  ---@source TextButton.lua
+  ---@type TextButton
+  TextButton = import("./TextButton"),
+  ---@source UiElement.lua
+  ---@type UiElement
+  UiElement = import("./UIElement"),
+  ValueLabel = import("./ValueLabel"),
 }
 
 return ui
