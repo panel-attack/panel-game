8a2ce3e97a6ef2082afcad73b7e7077c12210c9a
Brian Keegan <bkeegan@macbookpro.lan>
2026-05-10 01:45:56 -0400

    Raise default MAX_LAG and allow config override

--- client/src/globals.lua
+++ client/src/globals.lua
@@ -6,7 +6,13 @@ GARBAGE_TELEGRAPH_TIME = 45 -- the amount of time the garbage stays in the teleg
 GARBAGE_DELAY_LAND_TIME = 60 -- this is the amount of time after garbage leaves the telegraph before it can land on the opponent
 						  -- a higher value allows less rollback to happen and makes lag have less of an impact on the game
 						  -- technically this was 0 in classic games, but we are using this value to make rollback less noticable and match PA history
-MAX_LAG = 155 + GARBAGE_TELEGRAPH_TIME + GARBAGE_TRANSIT_TIME -- maximum amount of lag before net games abort
+local defaultDesyncTolerance = 230
+local configuredDesyncTolerance = config and tonumber(config.max_lag_frames)
+if configuredDesyncTolerance and configuredDesyncTolerance >= 1 then
+  MAX_LAG = math.floor(configuredDesyncTolerance)
+else
+  MAX_LAG = defaultDesyncTolerance + GARBAGE_TELEGRAPH_TIME + GARBAGE_TRANSIT_TIME -- maximum amount of lag before net games abort
+end
 NAME_LENGTH_LIMIT = 16
 
 themes = {} -- initialized in theme.lua
