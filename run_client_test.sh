4358b89379b87dce6c56628e77e3f73f70b88ba6
Brian Keegan <bkeegan@macbookpro.lan>
2026-05-15 20:10:26 -0400

    Patch 4

--- run_client_test.sh
+++ run_client_test.sh
@@ -7,10 +7,10 @@
 # Comment / uncomment lines to vary the bench.
 
 # Do not let each run_client invocation kill already launched clients.
-PA_KILL_EXISTING=false PA_NETWORK_PROFILE=sluggish_wifi    zsh run_client.sh Lala &
-PA_KILL_EXISTING=false PA_NETWORK_PROFILE=mobile_train     zsh run_client.sh Bevy &
-PA_KILL_EXISTING=false PA_NETWORK_PROFILE=transcontinental zsh run_client.sh Koozie &
-PA_KILL_EXISTING=false PA_NETWORK_PROFILE=satellite        zsh run_client.sh Hayley &
+# PA_KILL_EXISTING=false PA_NETWORK_PROFILE=sluggish_wifi    zsh run_client.sh Lala &
+# PA_KILL_EXISTING=false PA_NETWORK_PROFILE=mobile_train     zsh run_client.sh Bevy &
+# PA_KILL_EXISTING=false PA_NETWORK_PROFILE=transcontinental zsh run_client.sh Koozie &
+# PA_KILL_EXISTING=false PA_NETWORK_PROFILE=satellite        zsh run_client.sh Hayley &
 PA_KILL_EXISTING=false PA_NETWORK_PROFILE=pathological     zsh run_client.sh Amber &
 PA_KILL_EXISTING=false PA_NETWORK_PROFILE=pathological     zsh run_client.sh Brian &
 PA_KILL_EXISTING=false PA_NETWORK_PROFILE=pathological     zsh run_client.sh Gromit &
